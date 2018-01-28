215d9825264d60a84cc524b3360c8eedd099f324
aopstudio <cenzhedong2@126.com>
2018-01-28 16:52:10 +0800

    first commit

---
+++ vbcon1/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Collections
+
+Module Module1
+
+
+    Sub Main()
+        Dim a(5) As Double
+
+        For b = 1 To 5 Step 2
+            a(b) = b + 5
+            Console.WriteLine("{0}", a(b))
+
+        Next
+    End Sub
+End Module
---
+++ vbcon1/Rectangle.vb
@@ -0,0 +1,14 @@
+﻿Public Class Rectangle
+    Private length As Double
+    Private width As Double
+
+    'Public methods
+    Public Sub AcceptDetails()
+        length = 4.5
+        width = 3.5
+    End Sub
+
+    Public Function GetArea() As Double
+        GetArea = length * width
+    End Function
+End Class
